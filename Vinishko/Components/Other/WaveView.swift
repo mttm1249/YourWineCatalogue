1016a022f725e5eae6e2f08ffd9ca6c470ff14a6
mttm1249 <scumboy88@gmail.com>
2023-11-18 13:58:06 +0300

    wine waves

---
+++ Vinishko/Components/Other/WaveView.swift
@@ -0,0 +1,64 @@
+import SwiftUI
+
+struct WaveAnimation<Content: View>: View {
+    @State private var waveHeightPercent = 10.0
+    @State private var waveOffset = Angle(degrees: 0)
+    private let color = Pallete.redWineColor
+    let content: Content
+
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+
+    var body: some View {
+        ZStack {
+            Wave(offSet: Angle(degrees: waveOffset.degrees), percent: waveHeightPercent)
+                .fill(color)
+                .ignoresSafeArea(.all)
+            
+            content
+        }
+        .onAppear {
+            withAnimation(.linear(duration: 1.5).repeatForever(autoreverses: false)) {
+                self.waveOffset = Angle(degrees: 360)
+            }
+        }
+    }
+}
+
+struct Wave: Shape {
+    
+    var offSet: Angle
+    var percent: Double
+    
+    var animatableData: Double {
+        get { offSet.degrees }
+        set { offSet = Angle(degrees: newValue) }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var p = Path()
+        
+        let lowestWave = 0.02
+        let highestWave = 1.00
+        
+        let newPercent = lowestWave + (highestWave - lowestWave) * (percent / 100)
+        let waveHeight = 0.015 * rect.height
+        let yOffSet = CGFloat(1 - newPercent) * (rect.height - 4 * waveHeight) + 2 * waveHeight
+        let startAngle = offSet
+        let endAngle = offSet + Angle(degrees: 360 + 10)
+        
+        p.move(to: CGPoint(x: 0, y: yOffSet + waveHeight * CGFloat(sin(offSet.radians))))
+        
+        for angle in stride(from: startAngle.degrees, through: endAngle.degrees, by: 5) {
+            let x = CGFloat((angle - startAngle.degrees) / 360) * rect.width
+            p.addLine(to: CGPoint(x: x, y: yOffSet + waveHeight * CGFloat(sin(Angle(degrees: angle).radians))))
+        }
+        
+        p.addLine(to: CGPoint(x: rect.width, y: rect.height))
+        p.addLine(to: CGPoint(x: 0, y: rect.height))
+        p.closeSubpath()
+        
+        return p
+    }
+}
--- Vinishko/Modules/MainScreen/MainScreenView.swift
+++ Vinishko/Modules/MainScreen/MainScreenView.swift
@@ -10,42 +10,49 @@ import SwiftUI
 struct MainScreenView: View {
     @State var showSaveBanner = false
     @State var offset: CGFloat = 300
-    
+
     var body: some View {
         NavigationView {
-            VStack(spacing: 25) {
-                Spacer()
-                Text("Vinishko")
-                    .font(.title).bold()
-                    .foregroundColor(Pallete.mainColor)
-                    .padding(.bottom, 150)
-                NavigationButton(destination:
-                                    NewBottleScreen(viewModel: NewBottleViewModel(editableBottle: nil,
-                                                   context: CoreDataManager.managedContext)),
-                                 imageName: "plus")
-                NavigationButton(destination: BottlesCatalogueView()
-                    .environmentObject(BottlesCatalogueViewModel(context: CoreDataManager.managedContext)),
-                                 imageName: "list.star")
-                .padding(.bottom, 150)
-                Spacer()
-                if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String,
-                   let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String {
-                    Text("Версия приложения: \(version), сборка: \(build)")
-                        .font(.caption2)
-                }
-            }
-            .overlay(
-                VStack {
+            WaveAnimation {
+                VStack(spacing: 25) {
                     Spacer()
-                    Text("Сохранено!")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .frame(width: 200, height: 50)
-                        .background(Color.green)
-                        .cornerRadius(15)
-                        .offset(y: showSaveBanner ? 0 : offset)
+                    Text("Vinishko")
+                        .font(.title).bold()
+                        .foregroundColor(Pallete.mainColor)
+                        .padding(.bottom, 150)
+
+                    NavigationButton(destination:
+                                        NewBottleScreen(viewModel: NewBottleViewModel(editableBottle: nil,
+                                                       context: CoreDataManager.managedContext)),
+                                     imageName: "plus")
+                    
+                    NavigationButton(destination: BottlesCatalogueView()
+                        .environmentObject(BottlesCatalogueViewModel(context: CoreDataManager.managedContext)),
+                                     imageName: "list.star")
+                    .padding(.bottom, 150)
+                    
+                    Spacer()
+
+                    // Версия и сборка приложения
+                    if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String,
+                       let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String {
+                        Text("Версия приложения: \(version), сборка: \(build)")
+                            .font(.caption2)
+                    }
                 }
-            )
+                .overlay(
+                    VStack {
+                        Spacer()
+                        Text("Сохранено!")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .frame(width: 200, height: 50)
+                            .background(Color.green)
+                            .cornerRadius(15)
+                            .offset(y: showSaveBanner ? 0 : offset)
+                    }
+                )
+            }
             .onChange(of: showSaveBanner) { newValue in
                 if newValue {
                     withAnimation {
